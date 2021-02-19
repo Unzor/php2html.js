85599f10cccafec329c4d73fd2f51b3ecfdc02a5
Unzor <71789103+Unzor@users.noreply.github.com>
2021-02-18 21:21:48 -0600

    Create demo.php

---
+++ src/demo.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body style="background-color: white;">
+
+<?php
+echo "php2html Demo";
+?> 
+<p>
+  
+  </p>
+  <b>
+  The sentence above me was made with PHP.
+  </b>
+ <p>
+  
+  </p>
+  <a href="https://php2html.glitch.me/demo.php" download>Download This Page</a>
+</body>
+</html>
